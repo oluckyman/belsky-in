24d13af8e515cfdf76419d4820a23c8a3b647f86
oluckyman-pro <oluckyman@example.com>
2015-01-27 20:33:35 +0100

    Add Happy Birthday Mom site

--- web.js
+++ web.js
@@ -2,13 +2,18 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-    response.send('Hello <b>Keshee</b>!');
+    response.sendfile('./index.html');
 });
 
-app.get('/hbm', function(request, response) {
-    response.send('Here is mom birthday page');
+// Happy Birthday, Mom 2015
+//
+app.use('/hbm2015/', express.static(__dirname + '/hbm2015'));
+app.get(/\/hbm2015$/, function(request, response) {
+    response.redirect('/hbm2015/');
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
