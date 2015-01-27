950e8f53d1c5a0f8b8e41f2ca817722af2ed4f01
oluckyman-pro <oluckyman@example.com>
2015-01-27 15:32:44 +0100

    Bold keshee

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-    response.send('Hello Keshee!');
+    response.send('Hello <bKeshee</b>!');
 });
 
 var port = process.env.PORT || 5000;
