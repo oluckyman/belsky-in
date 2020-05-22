3f94a01eed78846618a679a7b7728f5de70cd350
ilya <ilya@belsky.in>
2020-05-22 18:16:36 +0200

    Serve index.html

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,15 @@ import logger from './log'
 
 const app: express.Application = express()
 
+// app.use(express.static(__dirname))
 app.get('/', (req, res) => {
-  res.json(JSON.stringify({ok: new Date()})).end()
+  res.sendFile('index.html', { root: __dirname });
 })
 
+
 const port = process.env.PORT || 5000
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info('Listening http://localhost:%s', port)
+  // tslint:disable-next-line:no-console
+  console.log(server.address())
 })
