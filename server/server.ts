ef7a1987a70f081ead10972926d11cceb359ebdd
Ilya Belsky <elias.logins@gmail.com>
2022-10-21 18:13:52 +0200

    Remove heroku deps, get ready for vercel

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import axios from 'axios'
 import dotenv from 'dotenv'
 import logger from './log'
 
@@ -7,36 +6,18 @@ dotenv.config()
 
 const app: express.Application = express()
 
-// app.use(express.static(__dirname))
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.sendFile('index.html', { root: __dirname });
 })
 
-app.get('/cv.pdf', (req, res) => {
+app.get('/cv.pdf', (_req, res) => {
   res.sendFile('cv.pdf', { root: __dirname });
 })
 
-const { PORT = 5000, HOST, NODE_ENV = 'production' } = process.env
+const { PORT = 3000, NODE_ENV = 'production' } = process.env
 app.listen(PORT, () => {
   if (NODE_ENV === 'development') {
     logger.info(`Listening http://localhost:${PORT}`)
-  }
-  if (NODE_ENV === 'production') {
-    // Prevent app from sleeping
-    app.get('/ping', (req, res) => res.send('pong'))
-    const ping = () => axios.get(`https://${HOST}/ping`)
-      .then(response => {
-        logger.info(response.data)
-      })
-      .catch(error => {
-        logger.error(error)
-      })
-      .then(() => {
-        setTimeout(ping, 1000 * 60 * 25)
-      })
-
-    logger.info('Ping myself 👈')
-    ping()
   }
 })
 
